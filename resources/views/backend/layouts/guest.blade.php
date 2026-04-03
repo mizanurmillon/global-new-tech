9adf5fe41ed2b8acfd81c42c14c0b065ff35c326
MD. RAHATUL RABBI <learnwithfair@gmail.com>
2026-04-03 17:28:10 +0600

    blog api servce done

--- resources/views/backend/layouts/guest.blade.php
+++ resources/views/backend/layouts/guest.blade.php
@@ -3,14 +3,16 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="description" content="Aero Hire">
-    <meta name="keywords" content="Aero Hire, Aero-Hire">
+    <meta name="description"
+        content="globalnewtech, global new tech, globalnewtech.com, globalnewtechbd, global new tech bd, globalnewtech.com.bd, global new tech com bd,">
+    <meta name="keywords"
+        content="rahatul rabbi, rahatul, rabbi, rahatul-rabbi, rahatul_rabbi, md rahatul rabbi, web developer, full stack developer, laravel developer, php developer, javascript developer, front end developer,">
     <meta name="author" content="Rahatul Rabbi, Rahatul, Rabbi, Rahatul-Rabbi, Rahatul_Rabbi, MD RAHATUL RABBI">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{ asset($systemSetting->favicon ?? 'backend/assets/images/logo-minimize.svg') }}">
-    <title>{{ $systemSetting->system_name  }}@yield('title')</title>
+    <title>{{ $systemSetting->system_name }}@yield('title')</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
